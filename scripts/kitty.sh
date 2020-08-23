945b2b4244bafd35bf82a6ae683d2bb193f21674
Santos Gallegos <stsewd@protonmail.com>
2020-08-23 18:37:25 -0500

    Automate kitty installation

--- scripts/kitty.sh
+++ scripts/kitty.sh
@@ -1,3 +1,9 @@
+# Update/install kitty
+# https://sw.kovidgoyal.net/kitty/binary.html
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
+
+
 # Install kitty themes
 
 THEME_DIR=~/.config/kitty/themes
