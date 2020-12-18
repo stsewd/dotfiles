d92ecbb9ed4fbb770f3f0417ae2b9bccdc9e50d8
Santos Gallegos <stsewd@protonmail.com>
2020-12-18 00:31:47 -0500

    Install requirements for pyenv

--- scripts/pyenv.sh
+++ scripts/pyenv.sh
@@ -2,3 +2,11 @@
 # https://github.com/pyenv/pyenv-installer
 
 curl https://pyenv.run | bash
+
+# Pyenv requirements
+sudo dnf install -y \
+    make gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel
+
+# Create neovim environment
+pyenv install 3.9.1
+pyenv virtualenv 3.9.1 neovim
