0eea5c3791a69fd6a2cd56c751e879144bf97372
Santos Gallegos <stsewd@protonmail.com>
2021-01-08 17:43:50 -0500

    More docker steps

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,4 +1,8 @@
-sudo dnf install -y moby-engine
+sudo dnf install -y moby-engine docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+# Change cgroups and networking
 sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
+sudo firewall-cmd --permanent --zone=trusted --add-interface=docker0
+sudo firewall-cmd --permanent --zone=FedoraWorkstation --add-masquerade
