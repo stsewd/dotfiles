4be10ab284cc9f7e3aebc03a6cd5635f7aa0c46a
Santos Gallegos <stsewd@protonmail.com>
2021-03-27 00:21:37 -0500

    Fix path

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,8 +1,8 @@
-TARGET_DIR=~/.config/systemd/
+TARGET_DIR=~/.config/systemd/user
 if [[ ! -d "$TARGET_DIR" ]]; then
   mkdir -p $TARGET_DIR
 fi
 
-ln -sf ~/dotfiles/config/systemd/user/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
+ln -sf ~/dotfiles/config/systemd/user/ssh-agent.service $TARGET_DIR/ssh-agent.service
 systemctl --user enable ssh-agent
 systemctl --user start ssh-agent
