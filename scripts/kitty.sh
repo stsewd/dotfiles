60788d706168ba924d3cbe451dd98260f1c969cd
Santos Gallegos <stsewd@protonmail.com>
2021-05-02 11:26:29 -0500

    Kitty: use tokyonight as theme

--- scripts/kitty.sh
+++ scripts/kitty.sh
@@ -9,3 +9,7 @@ fi
 wget \
   https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/${THEME} \
   -O "${THEME_DIR}/${THEME}"
+
+wget \
+  https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/kitty_tokyonight_storm.conf \
+  -O "${THEME_DIR}/tokyonight-storm.conf"
