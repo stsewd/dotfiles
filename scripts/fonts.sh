626d019b8b09a57f52534b0bdb73548f51217fc6
Santos Gallegos <stsewd@protonmail.com>
2022-06-11 15:04:26 -0500

    Update fonts script

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -6,10 +6,8 @@ wget -O /tmp/fonts.zip https://download.jetbrains.com/fonts/${FONT_NAME}
 unzip -od /tmp/fonts/ /tmp/fonts.zip
 
 FONTS_DIR=/tmp/fonts/fonts/ttf
-FONTS_TARGET_DIR=~/.local/share/fonts/
-if [[ ! -d "$FONTS_TARGET_DIR" ]]; then
-  mkdir -p $FONTS_TARGET_DIR
-fi
+FONTS_TARGET_DIR=$HOME/.local/share/fonts/
+mkdir -p $FONTS_TARGET_DIR
 
 cp -v \
   $FONTS_DIR/JetBrainsMono-Regular.ttf \
@@ -17,3 +15,5 @@ cp -v \
   $FONTS_DIR/JetBrainsMono-Italic.ttf \
   $FONTS_DIR/JetBrainsMono-BoldItalic.ttf \
   $FONTS_TARGET_DIR
+
+wget --directory-prefix=$FONTS_TARGET_DIR https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
