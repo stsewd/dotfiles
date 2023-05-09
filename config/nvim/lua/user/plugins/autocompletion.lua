bd0eb4ed1506ba9d30a781fb5a7fd46167dc9b1e
Santos Gallegos <stsewd@proton.me>
2023-05-08 21:06:25 -0500

    Testing lsp-config

--- config/nvim/lua/user/plugins/autocompletion.lua
+++ config/nvim/lua/user/plugins/autocompletion.lua
@@ -1,71 +1,117 @@
 local map = vim.keymap.set
 return {
-  "honza/vim-snippets",
   "github/copilot.vim",
+  -- General autocompletion
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  -- Snippets
+  "SirVer/ultisnips",
+  "honza/vim-snippets",
+  "quangnguyen30192/cmp-nvim-ultisnips",
+
+  -- LSP
+  "hrsh7th/cmp-nvim-lsp",
   {
-    "neoclide/coc.nvim",
-    build = "yarn install --frozen-lockfile",
-    init = function()
-      vim.g.coc_global_extensions = {
-        "coc-word",
-        "coc-syntax",
-        "coc-dictionary",
-        "coc-snippets",
-        "coc-emoji",
-        "coc-json",
-        "coc-yaml",
-        "coc-toml",
-        "coc-html",
-        "coc-css",
-        "coc-pyright",
-        "coc-rust-analyzer",
-        "coc-tsserver",
-        "coc-tag",
-        "coc-sumneko-lua",
-        "coc-clangd",
-        "coc-esbonio",
-      }
-    end,
+    "neovim/nvim-lspconfig",
     config = function()
-      vim.o.tagfunc = "CocTagFunc"
-      vim.cmd([[
-        augroup CocAutocomands
-          autocmd!
-          " Show function signature while completing args
-          autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-          autocmd FileType
-                \ json,js,html,c,cpp,h,hh
-                \ setlocal formatexpr=CocAction('formatSelected')
-        augroup END
-      ]])
+      map("n", "<C-k>", vim.diagnostic.goto_prev)
+      map("n", "<C-j>", vim.diagnostic.goto_next)
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-      map("n", "K", ":call CocAction('doHover')<CR>", { silent = true, desc = "Hover" })
-      map("n", "gK", "K", { desc = "Search man/help page" })
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set("n", "<space>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<space>f", function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+        end,
+      })
 
-      map("i", "<CR>", function()
-        if vim.fn["coc#pum#visible"]() ~= 0 and vim.fn["coc#pum#info"]()["index"] ~= -1 then
-          return vim.fn["coc#pum#confirm"]()
-        end
-        return "<C-g>u<CR><C-r>=coc#on_enter()<CR>"
-      end, { expr = true, silent = true })
-      map("i", "<C-space>", "coc#refresh()", { expr = true, silent = true })
-      map("i", "<C-j>", "<Plug>(coc-snippets-expand-jump)", { silent = true })
-      map("n", "<C-k>", "<Plug>(coc-diagnostic-prev)", { silent = true })
-      map("n", "<C-j>", "<Plug>(coc-diagnostic-next)", { silent = true })
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      require("lspconfig").pyright.setup({
+        capabilities = capabilities,
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-j>"] = cmp.mapping(function(fallback)
+            cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+          end, {
+            "i",
+            "s",
+          }),
+          ["<C-k>"] = cmp.mapping(function(fallback)
+            cmp_ultisnips_mappings.jump_backwards(fallback)
+          end, {
+            "i",
+            "s",
+          }),
+        }),
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            vim.fn["UltiSnips#Anon"](args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "ultisnips" },
+          { name = "path" },
+        }),
+      })
+      -- `/` cmdline setup.
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "buffer" },
+        }),
+      })
 
-      map("n", "<C-w><leader>", "<Plug>(coc-float-jump)", { silent = true })
-
-      map("n", "<leader>cc", ":CocCommand<CR>", { silent = true })
-      map("n", "<leader>cg", "<Plug>(coc-definition)", { silent = true })
-      map("n", "<leader>ca", "<Plug>(coc-codeaction-cursor)", { silent = true })
-      map("x", "<leader>ca", "<Plug>(coc-codeaction-selected)", { silent = true })
-      map("n", "<leader>cs", "<Plug>(coc-codeaction-source)", { silent = true })
-      map("n", "<leader>r", "<Plug>(coc-references)", { silent = true })
-      map("n", "<leader>cl", "<Plug>(coc-codelens-action)", { silent = true })
-      map("n", "<leader>cf", "<Plug>(coc-format)", { silent = true })
-      map("n", "gr", "<Plug>(coc-rename)", { silent = true })
-      map("n", "qf", "<Plug>(coc-fix-current)", { silent = true })
-      map("n", "<leader>cd", ":call CocAction('diagnosticToggle')<CR>", { silent = true })
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
     end,
   },
 }
