99bbf1d29bbde1a2c730f273991a721a00b740fe
Santos Gallegos <stsewd@proton.me>
2023-08-25 13:57:04 -0500

    Remove unused mappings

--- config/nvim/lua/user/mappings.lua
+++ config/nvim/lua/user/mappings.lua
@@ -19,10 +19,6 @@ map("n", "<down>", "<C-e>", { desc = "Scroll down" })
 map("n", "<left>", "zh", { desc = "Scroll left" })
 map("n", "<right>", "zl", { desc = "Scroll right" })
 
--- Search
-map("n", "*", "*``", { desc = "Search word under cursor without moving the cursor" })
-map("n", "#", "#``", { desc = "Search word under cursor without moving the cursor" })
-
 -- Terminal mode mappings
 map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode with escape" })
 map("t", "<C-[>", "<C-\\><C-n>", { desc = "Exit terminal mode with escape" })
