d3446cd5e2b597d6827dc409c755650a100e44bc
Santos Gallegos <stsewd@proton.me>
2023-10-06 20:10:26 -0500

    Update plugins

--- config/nvim/lua/user/plugins/indent.lua
+++ config/nvim/lua/user/plugins/indent.lua
@@ -4,15 +4,22 @@ return {
     -- Show indentation lines
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-      require("indent_blankline").setup({
-        char = "¦",
-        filetype_exclude = { "help", "man", "", "text", "tsplayground", "rst", "markdown", "checkhealth" },
-        show_trailing_blankline_indent = false,
+      require("ibl").setup({
+        exclude = {
+          filetypes = { "help", "man", "text", "rst", "markdown", "checkhealth" },
+        },
+        indent = {
+          char = "▏",
+          -- char = "¦",
+        },
+        scope = {
+          enabled = false,
+        },
       })
 
       -- Workaround for https://github.com/lukas-reineke/indent-blankline.nvim/issues/449.
-      map("n", "za", "za<cmd>IndentBlanklineRefresh<CR>")
-      map("n", "zR", "zR<cmd>IndentBlanklineRefresh<CR>")
+      -- map("n", "za", "za<cmd>IndentBlanklineRefresh<CR>")
+      -- map("n", "zR", "zR<cmd>IndentBlanklineRefresh<CR>")
     end,
   },
 }
