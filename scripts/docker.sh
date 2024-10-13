a2b71bc40e49dda0a5c23ec137a3e5b3bf7d712c
Santos Gallegos <stsewd@proton.me>
2024-10-13 15:41:59 -0500

    Update plugins

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,4 +7,4 @@ sudo systemctl enable containerd.service
 # Change cgroups.
 # Isn't needed for the current version of docker,
 # but it's needed for gvisor.
-sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
+# sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
