ee22b78b66434f59e83638326f085b254ba84db9
Santos Gallegos <stsewd@proton.me>
2025-01-11 21:53:49 -0500

    Updates

--- scripts/luals.sh
+++ scripts/luals.sh
@@ -1,6 +1,6 @@
 # Install Lua Language Server
 
-VERSION=3.11.1
+VERSION=3.13.5
 wget \
   https://github.com/LuaLS/lua-language-server/releases/download/$VERSION/lua-language-server-$VERSION-linux-x64.tar.gz \
   -O /tmp/lua-language-server.tar.gz
