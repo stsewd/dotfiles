bff6321b98889cf1f064a28909fed461610c9ed7
Santos Gallegos <stsewd@proton.me>
2025-12-23 15:46:52 -0500

    Update versions

--- scripts/luals.sh
+++ scripts/luals.sh
@@ -1,6 +1,6 @@
 # Install Lua Language Server
 
-VERSION=3.15.0
+VERSION=3.16.3
 wget \
   https://github.com/LuaLS/lua-language-server/releases/download/$VERSION/lua-language-server-$VERSION-linux-x64.tar.gz \
   -O /tmp/lua-language-server.tar.gz
--- scripts/marksman.sh
+++ scripts/marksman.sh
@@ -1,3 +1,3 @@
-VERSION=2024-12-18
+VERSION=2025-12-13
 wget https://github.com/artempyanykh/marksman/releases/download/${VERSION}/marksman-linux-x64 -O ~/.local/bin/marksman
 chmod +x ~/.local/bin/marksman
