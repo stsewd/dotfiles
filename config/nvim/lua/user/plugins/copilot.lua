fef85662c0234147dc0e1639406e3c2aba043fcd
Santos Gallegos <stsewd@proton.me>
2026-06-06 17:32:34 -0500

    Updates

--- config/nvim/lua/user/plugins/copilot.lua
+++ config/nvim/lua/user/plugins/copilot.lua
@@ -9,30 +9,4 @@ return {
       map("i", "<c-l>", "<Plug>(copilot-accept-word)")
     end,
   },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    -- Load the plugin only when these commands are run.
-    cmd = {
-      "CopilotChat",
-      "CopilotChatToggle",
-      "CopilotChatDebugInfo",
-      "CopilotChatExplain",
-      "CopilotChatReview",
-      "CopilotChatFix",
-      "CopilotChatOptimize",
-      "CopilotChatDocs",
-      "CopilotChatTests",
-      "CopilotChatFixDiagnostic",
-      "CopilotChatCommit",
-      "CopilotChatCommitStaged",
-    },
-    branch = "canary",
-    dependencies = {
-      { "github/copilot.vim" },
-      { "nvim-lua/plenary.nvim" },
-    },
-    config = function()
-      require("CopilotChat").setup()
-    end,
-  },
 }
